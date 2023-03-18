b55b0f3394d3fba692e1a981eecd23c8475130b3
Kemal muhammad <kemalhiero@gmail.com>
2023-03-19 02:43:20 +0700

    simpan gambar dgn base64

--- controllers/panti_controller.js
+++ controllers/panti_controller.js
@@ -20,52 +20,66 @@ const deleteFile = (filePath) => {
 
 // ------------------------------------------------------
 const registerAdminPanti = async (req,res, next) =>{
-  try {
-          
-    const {email, id_panti, nik, nama, nohp, jenis_kelamin, tempat_lahir, tgl_lahir, alamat, password, konfirmasiPassword} = req.body;
-    const foto = req.file.filename;
-    
-    // cek password sudah sama dengan konfirmasi password
-    if (password !== konfirmasiPassword) {
-      deleteFile('./uploads/pp/' + foto)
-      return res.status(422).json({ error: true, message: 'Password dan Confirm Password tidak cocok' })
-    } 
-    
-    console.log("ini emailnya aaaaa " + id_panti + nik + nama + foto)
-    
-    // cek sudah ada email sudah digunakan atau belum
-    const adaEmail = await modelUser.findOne({ where: { email } });
-    if (adaEmail) {
-      deleteFile('./uploads/pp/' + foto)
-      return res.status(409).json({ error: true, message: 'Email sudah digunakan' });
+    try {
+      const {email, password, konfirmasiPassword } = req.body;
+
+      // cek password sudah sama dengan konfirmasi password
+      if (password !== konfirmasiPassword) {
+        // deleteFile(filePath)
+        return res.status(422).json({ error: true, message: 'Password dan Confirm Password tidak cocok' })
+      } 
+      
+      
+      // cek sudah ada email sudah digunakan atau belum
+      const adaEmail = await modelUser.findOne({ where: { email } });
+      if (adaEmail) {
+        // deleteFile(filePath)
+        return res.status(409).json({ error: true, message: 'Email sudah digunakan' });
+      }
+            
+      const {id_panti, nik, nama, no_hp, jenis_kelamin, tempat_lahir, tgl_lahir, alamat, foto} = req.body;
+
+      // memberi nama file
+      const ekstensi_file = foto.split(';')[0].split('/')[1];
+      const filename = Date.now().toString();
+      const filePath = `./uploads/pp/${filename}.${ekstensi_file}`;
+      const content = foto.split('base64,')[1]
+
+      fs.writeFile(filePath, content, 'base64', (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(`File ${filename}.${ekstensi_file} berhasil disimpan.`);
+        }
+      });
+      
+      // hash password
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      // masukin ke database
+      const userBaru = await modelUser.create({
+          email, nik, nama, jenis_kelamin, alamat, tempat_lahir, tgl_lahir, no_hp, 
+          foto: `${filename}.${ekstensi_file}`,
+          password:hashedPassword,
+          role: 'admin_panti',
+          status_id: '1'
+      })
+
+      const adminBaru = await modelDetailAdminPanti.create({
+          id_panti, email
+      })
+      
+      if ( foto && userBaru && adminBaru) {
+          return res.status(201).json({ error: false, message: 'User berhasil registrasi' });
+      } else {
+          console.error("gagal upload")
+          deleteFile(filePath)
+      }
+
+    } catch (error) {
+        res.status(500).json({ error:true, message: error });
     }
-
-    // hash password
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    // masukin ke database
-    const userBaru = await modelUser.create({
-        email, nik, nama, jenis_kelamin, alamat, tempat_lahir, tgl_lahir, nohp, foto,
-        password:hashedPassword,
-        role: 'admin_panti',
-        status_id: '1'
-    })
-
-    const adminBaru = await modelDetailAdminPanti.create({
-        id_panti, email
-    })
-    
-    if ( foto && userBaru && adminBaru) {
-        return res.status(201).json({ error: false, message: 'User berhasil registrasi' });
-    } else {
-        console.error("gagal upload")
-        deleteFile('./uploads/pp/' + foto)
-    }
-
-} catch (error) {
-    res.status(500).json({ error:true, message: error });
-}
 
 }
 
